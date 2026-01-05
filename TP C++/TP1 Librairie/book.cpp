3ff2e3b6c8ea21ca6a925b2485dd10d11bb2ce9a
FereroPayne <160160367+FereroPayne@users.noreply.github.com>
2026-01-05 09:35:26 +0100

    Add Bibliothèque class for library management

---
+++ TP C++/TP1 Librairie/book.cpp
@@ -0,0 +1,35 @@
+#ifndef admin_Biblio_h
+#define admin_Biblio_h
+#include <string>
+
+class Bibliothèque {
+    public:                                                
+        Bibliothèque(
+            std::string nom,
+            std::string adresse,
+            std::string IDbiblio
+        );
+
+        std::string getNom() const;
+        std::string getAdresse() const;
+        std::string getIDbiblio() const;
+
+        void AjouterLivre();
+        void SupprimerLivre();
+        void RechercherLivre();
+        void AjouterAuteur();
+        void AjouterLecteur();
+    Lecteur emprunter(){
+        
+        
+
+    }
+           
+
+    private:
+        std::string nom;
+        std::string adresse;
+        std::string IDbiblio;
+
+};
+
